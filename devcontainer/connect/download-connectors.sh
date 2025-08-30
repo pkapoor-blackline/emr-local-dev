ad6844216b3941027bacf1ad6047d7f594652e12
pkapoor <pkapoor@blacklinesafety.com>
2025-08-29 22:35:10 -0400

    Devcontainer does not work, so just use containers

--- devcontainer/connect/download-connectors.sh
+++ devcontainer/connect/download-connectors.sh
@@ -18,7 +18,7 @@ curl -S -L ${DOWNLOAD_URL} | tar -C ${SRC_PATH} --warning=no-unknown-keyword -xz
 
 ## Confluent S3 Sink Connector
 echo "downloading confluent s3 connector..."
-DOWNLOAD_URL=https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-s3/versions/10.0.5/confluentinc-kafka-connect-s3-10.0.5.zip
+DOWNLOAD_URL=https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-s3/versions/10.0.5/confluentinc-kafka-connect-s3-10.0.5.zip
 
 curl ${DOWNLOAD_URL} -o ${SRC_PATH}/confluent.zip \
   && unzip -qq ${SRC_PATH}/confluent.zip -d ${SRC_PATH} \
@@ -26,17 +26,18 @@ curl ${DOWNLOAD_URL} -o ${SRC_PATH}/confluent.zip \
   && mv ${SRC_PATH}/$(ls ${SRC_PATH} | grep confluentinc-kafka-connect-s3) ${SRC_PATH}/confluent-s3
 
 ## Voluble Source Connector
-echo "downloading voluble connector..."
-DOWNLOAD_URL=https://d1i4a15mxbxib1.cloudfront.net/api/plugins/mdrogalis/voluble/versions/0.3.1/mdrogalis-voluble-0.3.1.zip
+# echo "downloading voluble connector..."
+# DOWNLOAD_URL=https://d1i4a15mxbxib1.cloudfront.net/api/plugins/mdrogalis/voluble/versions/0.3.1/mdrogalis-voluble-0.3.1.zip
+# DOWNLOAD_URL=https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/voluble/versions/0.3.1/confluentinc-voluble-0.3.1.zip
 
-curl ${DOWNLOAD_URL} -o ${SRC_PATH}/voluble.zip \
-  && unzip -qq ${SRC_PATH}/voluble.zip -d ${SRC_PATH} \
-  && rm ${SRC_PATH}/voluble.zip \
-  && mv ${SRC_PATH}/$(ls ${SRC_PATH} | grep mdrogalis-voluble) ${SRC_PATH}/voluble
+# curl ${DOWNLOAD_URL} -o ${SRC_PATH}/voluble.zip \
+#   && unzip -qq ${SRC_PATH}/voluble.zip -d ${SRC_PATH} \
+#   && rm ${SRC_PATH}/voluble.zip \
+#   && mv ${SRC_PATH}/$(ls ${SRC_PATH} | grep mdrogalis-voluble) ${SRC_PATH}/voluble
 
 if [ ${WITH_AVRO} == "Y" ]; then
   echo "downloading kafka connect avro converter..."
-  DOWNLOAD_URL=https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-avro-converter/versions/6.0.3/confluentinc-kafka-connect-avro-converter-6.0.3.zip
+  DOWNLOAD_URL=https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-avro-converter/versions/6.0.3/confluentinc-kafka-connect-avro-converter-6.0.3.zip
 
   curl ${DOWNLOAD_URL} -o ${SRC_PATH}/avro.zip \
     && unzip -qq ${SRC_PATH}/avro.zip -d ${SRC_PATH} \
@@ -46,5 +47,5 @@ if [ ${WITH_AVRO} == "Y" ]; then
   echo "copying to connectors..."
   cp -r ${SRC_PATH}/avro/lib/* ${SRC_PATH}/debezium-connector-postgres
   cp -r ${SRC_PATH}/avro/lib/* ${SRC_PATH}/confluent-s3/lib
-  cp -r ${SRC_PATH}/avro/lib/* ${SRC_PATH}/voluble/lib
+  # cp -r ${SRC_PATH}/avro/lib/* ${SRC_PATH}/voluble/lib
 fi
